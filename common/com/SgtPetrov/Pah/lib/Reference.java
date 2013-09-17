ef486f9631465f11f9943ca5e5572e338156c9f7
SgtPetrov <Sgt.Petrov@gmail.com>
2013-09-17 16:16:35 +0200

    Saved Learn modding with Pahimar on Github.

---
+++ common/com/SgtPetrov/Pah/lib/Reference.java
@@ -0,0 +1,7 @@
+package com.SgtPetrov.Pah.lib;
+
+public class Reference {
+    public static final String MOD_ID = "Pah";
+    public static final String MOD_NAME = "Learn Modding with Pahimar";
+    public static final String VERSION = "0.0.1";
+}
